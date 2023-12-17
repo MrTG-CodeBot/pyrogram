009c982a5e34e7a8f3567ea680f473050c85e73f
KurimuzonAkuma <arturgayauskas@gmail.com>
2023-12-18 00:59:23 +0300

    Add update_color to compiler

--- compiler/docs/compiler.py
+++ compiler/docs/compiler.py
@@ -267,6 +267,7 @@ def pyrogram_api():
             get_common_chats
             get_default_emoji_statuses
             set_emoji_status
+            update_status
         """,
         invite_links="""
         Invite Links
