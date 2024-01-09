da4b467e8ff8118326ce968f83690f5060b1d091
aoleg94 <aoleg94@users.noreply.github.com>
2024-01-10 01:26:34 +0500

    Add FloodWait except to media download (#19)
    Add FloodWait except to media download to handle this error from the script.
    
    Before this change the exception would only be printed and the script would continue without a way to detect this error.
    
    Co-authored-by: KNF Apps <jordyamc@users.noreply.github.com>

--- pyrogram/client.py
+++ pyrogram/client.py
@@ -802,6 +802,9 @@ class Client(Methods):
             if isinstance(e, asyncio.CancelledError):
                 raise e
 
+            if isinstance(e, pyrogram.errors.FloodWait):
+                raise e
+
             return None
         else:
             if in_memory:
@@ -1032,6 +1035,8 @@ class Client(Methods):
                     finally:
                         await cdn_session.stop()
             except pyrogram.StopTransmission:
+                raise
+            except pyrogram.errors.FloodWait:
                 raise
             except Exception as e:
                 log.exception(e)
